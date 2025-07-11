a345909cfd78e648280c414d07f8c37ed74d87ff
karl <enriquezkharl14@gmail.com>
2025-07-12 03:01:43 +0800

    [Feat] authentication

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+const isProtectedRoute = createRouteMatcher(['/']);
+
+export default clerkMiddleware(async (auth, request) => {
+  const { redirectToSignIn } = await auth();
+  if (isProtectedRoute(request)) {
+    return redirectToSignIn({ returnBackUrl: request.url });
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)'
+  ]
+};

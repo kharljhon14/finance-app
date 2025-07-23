91be8cce054b38b7fad386461efd67b803652a65
karl <enriquezkharl14@gmail.com>
2025-07-23 11:19:54 +0800

    [Feat] Adding of accounts

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,27 +3,33 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
-import { Plus } from 'lucide-react';
-import { columns, Payment } from './columns';
+import { Loader2, Plus } from 'lucide-react';
+import { columns } from './columns';
 import { DataTable } from '@/components/data-table';
-
-const dummyData: Payment[] = [
-  {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com'
-  },
-  {
-    id: '728e252f',
-    amount: 13,
-    status: 'success',
-    email: 'm@example2.com'
-  }
-];
+import { useGetAccounts } from '@/features/accounts/hooks/api/use-get-accounts';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function AccountsPage() {
   const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader className="gap-y-2 flex flex-col lg:flex-row lg:items-center lg:justify-between">
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[400px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -40,9 +46,9 @@ export default function AccountsPage() {
         <CardContent>
           <DataTable
             onDelete={() => {}}
-            filterKey="email"
+            filterKey="name"
             columns={columns}
-            data={dummyData}
+            data={accounts}
           />
         </CardContent>
       </Card>

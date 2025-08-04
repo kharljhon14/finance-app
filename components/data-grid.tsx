0d297f70a7b7fc8535980ed48d83201c2a626dc6
karl <enriquezkharl14@gmail.com>
2025-08-05 00:36:17 +0800

    [Feat] Data card

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,14 +1,30 @@
 'use client';
 
+import { useGetSummary } from '@/features/summary/hooks/api/use-get-summary';
 import { formatDateRange } from '@/lib/utils';
 import { useSearchParams } from 'next/navigation';
+import { FaPiggyBank } from 'react-icons/fa';
+import DataCard from './data-card';
 
 export default function DataGrid() {
+  const { data } = useGetSummary();
+
   const params = useSearchParams();
   const to = params.get('to') || undefined;
   const from = params.get('from') || undefined;
 
-  const dateRangeLabels = formatDateRange({ to, from });
+  const dateRangeLabel = formatDateRange({ to, from });
 
-  return <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8"></div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+      <DataCard
+        title="Remaining"
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        varint="default"
+        dateRange={dateRangeLabel}
+      />
+    </div>
+  );
 }

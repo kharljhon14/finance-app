6e90ff2bbe3c57700aa370650be80a53527e6990
karl <enriquezkharl14@gmail.com>
2025-08-05 10:30:08 +0800

    [Feat] Remove developmenmt mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true
+        }
+      }}
+    >
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <QueryProviders>
